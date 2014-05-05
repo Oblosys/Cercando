23d8ee40f7f77a51b10d2332e78b13210e3bf3c8
Oblosys <martijn@oblomov.com>
2014-05-05 12:47:21 +0200

    Fix lib path

--- scripts/compileRestartReaderServer.sh
+++ scripts/compileRestartReaderServer.sh
@@ -6,4 +6,4 @@ rm -rf ReaderServer/bin/readerServer
 
 javac -cp ReaderServer/lib/ltkjava-1.0.0.7-with-dependencies.jar -sourcepath ReaderServer/src -d ReaderServer/bin/ ReaderServer/src/readerServer/*.java
 
-java -cp /Users/martijn/git/Cercando/ReaderServer/bin:/Users/martijn/dev/Java/java-experiment/lib/ltkjava-1.0.0.7-with-dependencies.jar readerServer.Main
+java -cp ReaderServer/bin:ReaderServer/lib/ltkjava-1.0.0.7-with-dependencies.jar readerServer.Main
