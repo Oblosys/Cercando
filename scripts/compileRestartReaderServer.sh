e5d7b98e038e39e8e472a36109ed672b446da757
Oblosys <martijn@oblomov.com>
2014-05-06 17:36:33 +0200

    Fix extension to script call

--- scripts/compileRestartReaderServer.sh
+++ scripts/compileRestartReaderServer.sh
@@ -10,4 +10,4 @@ rm -rf ReaderServer/bin/readerServer
 
 javac -cp ReaderServer/lib/ltkjava-1.0.0.7-with-dependencies.jar -sourcepath ReaderServer/src -d ReaderServer/bin/ ReaderServer/src/readerServer/*.java
 
-scripts/restartReaderServer
+scripts/restartReaderServer.sh
