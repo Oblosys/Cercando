b9a1c09a3fa923cd99301d81e38916d09ac200d1
Oblosys <martijn@oblomov.com>
2014-05-08 15:29:52 +0200

    Stop script after git pull fail

--- scripts/updateCompileRestartReaderServer.sh
+++ scripts/updateCompileRestartReaderServer.sh
@@ -1,4 +1,11 @@
 export PATH=/usr/local/bin:$PATH
 cd ~/git/Cercando
+
+echo "Updating Cercando repository with git pull"
 git pull
+if [ "$?" -ne "0" ]; then
+    echo "ERROR: git pull failed, exiting script"
+    exit 1
+fi
+
 scripts/compileRestartReaderServer.sh $1
