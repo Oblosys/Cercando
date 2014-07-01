9ef9e18da1bbe7cbba69e3f41db92f456e919ce2
Oblosys <martijn@oblomov.com>
2014-07-01 18:24:24 +0200

    Obsolete, since we use launchd now

--- scripts/startGuardedReaderServer.sh
+++
@@ -1,9 +0,0 @@
-export PATH=/usr/local/bin:$PATH
-cd ~/git/Cercando
-
-runServer="java -cp ReaderServer/resources:ReaderServer/bin:ReaderServer/lib/ltkjava-1.0.0.7-with-dependencies.jar readerServer.Main"
-
-until $runServer $1 ; do
-    echo "Reader server crashed with exit code $?. Respawning.." >&2
-    sleep 1
-done
