067cb884e966af40aaf7215b0a34862bff79c930
Oblosys <martijn@oblomov.com>
2014-08-21 16:39:11 +0200

    Explicitly wait until NAS has mounted before starting Lucy server

--- scripts/startLucyServer.sh
+++ scripts/startLucyServer.sh
@@ -1,5 +1,8 @@
 export PATH=/usr/local/bin:$PATH
 cd ~/git/Cercando
 
+# make sure Synology NAS is mounted before starting the server
+./scripts/waitUntilNasMounted.sh
+
 # Start a normal server, and pass optional 'remoteReader' arg in $1
 node LucyServer/js/server/LucyServer.js $1
