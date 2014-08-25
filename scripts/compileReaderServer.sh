c8822a92eb93a2115f6464e9a44fe7d49c94d561
Oblosys <martijn@oblomov.com>
2014-08-25 13:24:55 +0200

    Add script compileReaderServer.sh

---
+++ scripts/compileReaderServer.sh
@@ -0,0 +1,13 @@
+export PATH=/usr/local/bin:$PATH
+cd ~/git/Cercando
+
+scripts/killReaderServer.sh
+
+rm -rf ReaderServer/bin/readerServer
+
+echo Compiling reader server
+javac -cp ReaderServer/lib/ltkjava-1.0.0.7-with-dependencies.jar -sourcepath ReaderServer/src -d ReaderServer/bin/ ReaderServer/src/readerServer/*.java
+if [ "$?" -ne "0" ]; then
+    echo "ERROR: compilation failed, exiting script"
+    exit 1
+fi
