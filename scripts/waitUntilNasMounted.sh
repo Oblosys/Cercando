8d18a1a8d174411f4fd59cf9b34096b5b4b79c93
Oblosys <martijn@oblomov.com>
2014-08-25 14:14:44 +0200

    Chane user martijn ~> NHMR

--- scripts/waitUntilNasMounted.sh
+++ scripts/waitUntilNasMounted.sh
@@ -1,7 +1,7 @@
 # Don't exit until mounted NAS is available through the symbolic link ~/lucyData
 # By checking the existence, automount should take care of mounting the volume.
 # This may fail a couple of times if the network is not available yet while booting.
-while [ ! -d /Users/martijn/lucyData/savedReaderEvents ]
+while [ ! -d /Users/NHMR/lucyData/savedReaderEvents ]
 do
    echo 'Synology NAS is not mounted yet, waiting 5 seconds.'
    sleep 5
