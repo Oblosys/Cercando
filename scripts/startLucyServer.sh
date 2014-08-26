2d6bc2ade9d9fccb9cbb931f1b9188e20fd6afc0
Oblosys <martijn@oblomov.com>
2014-08-26 16:24:10 +0200

    Don't check for NAS when using remote reader
    This means we're not running on the Mac mini.

--- scripts/startLucyServer.sh
+++ scripts/startLucyServer.sh
@@ -3,8 +3,10 @@ cd ~/git/Cercando
 
 echo "########## startLucyServer.sh: Starting Lucy server"
 
+if [ "$1" != "remoteReader" ]; then
 # make sure Synology NAS is mounted before starting the server
 ./scripts/waitUntilNasMounted.sh
+fi
 
 # Start a normal server, and pass optional 'remoteReader' arg in $1
 node LucyServer/js/server/LucyServer.js $1
