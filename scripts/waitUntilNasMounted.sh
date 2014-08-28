b61606863ae31f14d4d49a46cd97e84b4a6cd106
Oblosys <martijn@oblomov.com>
2014-08-28 16:31:24 +0200

    Show message when Synology NAS has been mounted succesfully

--- scripts/waitUntilNasMounted.sh
+++ scripts/waitUntilNasMounted.sh
@@ -1,10 +1,10 @@
 # Don't exit until mounted NAS is available through the symbolic link ~/lucyData
 # By checking the existence, automount should take care of mounting the volume.
 # This may fail a couple of times if the network is not available yet while booting.
-echo "Checking whether Synology NS has mounted"
+echo "Checking whether Synology NS has been mounted.."
 while [ ! -d /Users/NHMR/lucyData/savedReaderEvents ]
 do
-   echo "Synology NAS is not mounted yet, waiting 5 seconds."
+   echo "Not mounted yet, waiting 5 seconds.."
    sleep 5
 done
-
+echo "Done: Synology NAS has been mounted." 
