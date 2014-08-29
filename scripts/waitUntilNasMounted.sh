5ccd9799869277f887c66bce66334226504f2c83
Oblosys <martijn@oblomov.com>
2014-08-29 14:56:56 +0200

    Adapt script to new Lucy data directory path

--- scripts/waitUntilNasMounted.sh
+++ scripts/waitUntilNasMounted.sh
@@ -1,8 +1,8 @@
-# Don't exit until mounted NAS is available through the symbolic link ~/lucyData
+# Don't exit until mounted NAS is available through the symbolic link ~/lucy/data
 # By checking the existence, automount should take care of mounting the volume.
 # This may fail a couple of times if the network is not available yet while booting.
 echo "Checking whether Synology NS has been mounted.."
-while [ ! -d /Users/NHMR/lucyData/savedReaderEvents ]
+while [ ! -d /Users/NHMR/lucy/data/savedReaderEvents ]
 do
    echo "Not mounted yet, waiting 5 seconds.."
    sleep 5
