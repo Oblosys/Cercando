79a27bbab51f36074bcaae6aeeb64495589ab173
Oblosys <martijn@oblomov.com>
2014-09-02 18:16:25 +0200

    Pass args to stop/startReaderServer.sh (in case of --daemon)

--- scripts/restartReaderServer.sh
+++ scripts/restartReaderServer.sh
@@ -1,8 +1,7 @@
 export PATH=/usr/local/bin:$PATH
 cd ~/git/Cercando
 
-scripts/stopReaderServer.sh
+scripts/stopReaderServer.sh $*
 # Give the server some time to gracefully disconnect from the readers
 sleep 1
-scripts/startReaderServer.sh
- 
+scripts/startReaderServer.sh $*
