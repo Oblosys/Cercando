a3000601cefe62b902187a1856793a674b3817bc
Martijn Schrage <martijn@oblomov.com>
2015-01-19 20:45:20 +0100

    Make TypeScript compile script as strict as Eclipse TypeScript settings

--- scripts/compileLucyServer.sh
+++ scripts/compileLucyServer.sh
@@ -3,21 +3,21 @@ cd ~/git/Cercando
 
 # This script assumes Lucy Server is not running
 echo "Compiling LucyServer"
-tsc -m commonjs -t ES5 LucyServer/js/server/LucyServer.ts
+tsc -m commonjs -t ES5 --noImplicitAny LucyServer/js/server/LucyServer.ts
 if [ "$?" -ne "0" ]; then
     echo "Typescript error in LucyServer"
     exit 1
 fi
 
 echo Compiling Client scripts
-tsc -m commonjs -t ES5 LucyServer/js/client/Locator.ts
+tsc -m commonjs -t ES5 --noImplicitAny LucyServer/js/client/Locator.ts
 if [ "$?" -ne "0" ]; then
     echo "Typescript error in Locator"
     exit 1
 fi
 
 echo Compiling user manager
-tsc -m commonjs -t ES5 LucyServer/js/tools/UserManager.ts
+tsc -m commonjs -t ES5 --noImplicitAny LucyServer/js/tools/UserManager.ts
 if [ "$?" -ne "0" ]; then
     echo "Typescript error in UserManager"
     exit 1
